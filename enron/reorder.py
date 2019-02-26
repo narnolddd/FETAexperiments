e6891f2df573b0122f70a3e5bbe52b81ee5b050b
Naomi Arnold <n.a.arnold@qmul.ac.uk>
2019-02-26 17:00:54 +0000

    updating experiments folder

---
+++ enron/reorder.py
@@ -0,0 +1,19 @@
+from operator import itemgetter
+file = "out.enron"
+file2 = "enron.dat"
+
+data=[]
+
+with open(file,'r') as f:
+    for i, line in enumerate(f):
+        if i == 0:
+            continue
+        parts=line.strip().split()
+        data.append(dict(src=parts[0],dst=parts[1],time=int(parts[3])))
+
+reordered = sorted(data, key = itemgetter('time'))
+
+with open(file2,'w') as f2:
+    for line in reordered:
+        f2.write(line['src']+" "+line['dst']+" "+str(line['time'])+"\n")
+    f2.close()
--- getMLEs.py
+++ getMLEs.py
@@ -1,11 +1,14 @@
 import numpy as np
 import math
 import matplotlib.pyplot as plt
+from textwrap import wrap
 
-MLEtable = np.zeros((9,10),dtype=float)
+experiments=50
 
-for ex in range(10):
-    file = "results/RankBA1000results"+str(ex)+".dat"
+MLEtable = np.zeros((9,experiments),dtype=float)
+
+for ex in range(experiments):
+    file = "fromLarry/BARank1000results"+str(ex)+".dat"
     lines=[]
     with open(file,'r') as f:
         while True:
@@ -20,13 +23,35 @@ for ex in range(10):
     for i in range(9):
         MLEtable[i,ex]=(np.argmax(params[i])+1)*10
 
-means = [sum(row)/10 for row in MLEtable]
-sds = [math.sqrt(sum((MLEtable[i,:] - means[i])**2)/10) for i in range(9)]
+means = [sum(row)/experiments for row in MLEtable]
+sds = [math.sqrt(sum((MLEtable[i,:] - means[i])**2)/experiments) for i in range(9)]
 truevals = range(100,1000,100)
+mse = [math.sqrt(sum((truevals[j] - MLEtable[j,:])**2)/experiments) for j in range(9)]
+kse = [max(abs(truevals[j]-MLEtable[j,:])) for j in range(9)]
 
-fig, ax = plt.subplots()
-ax.errorbar(truevals,means,yerr=sds, fmt='o')
-plt.plot(truevals,truevals, linestyle='-', linewidth=1)
-ax.set_title('Estimated changepoint times from Rank-preference to BA against true values')
+fig, (ax0,ax1) = plt.subplots(nrows=2, sharex=True)
 
+ax0.yaxis.label.set_fontsize(12)
+ax1.yaxis.label.set_fontsize(12)
+ax0.xaxis.label.set_fontsize(12)
+ax1.xaxis.label.set_fontsize(12)
+
+#fig.suptitle('Barabasi-Albert to Rank Preference')
+plt.xlabel('Iteration number corresponding to changepoint')
+
+ax0.errorbar(truevals,means,yerr=sds, fmt='o')
+ax0.plot(truevals,truevals, linestyle='--', linewidth=1, label="Correct changepoint time")
+#ax0.set_title('Estimated change point times (averaged over 50 experiments)')
+ax0.set_ylabel('MLE of change point time \n (# iterations)')
+
+#ax2.plot(truevals,kse, marker='o')
+#ax2.set_ylim(0,200)
+#ax2.set_title('KS error')
+
+ax1.plot(truevals,mse, marker='o')
+#ax1.set_title('Estimation error')
+ax1.set_ylim(0,20)
+ax1.set_ylabel('RMSE (# iterations)')
+
+plt.tight_layout(h_pad=0)
 plt.show()
