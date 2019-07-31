335c89665719614327a112650c10d593d8f3caf1
Naomi Arnold <n.a.arnold@qmul.ac.uk>
2019-07-31 10:49:57 +0100

    Similarity values update

--- BArankpref/AverageStats.py
+++ BArankpref/AverageStats.py
@@ -1,10 +1,12 @@
 import numpy as np
-BAstats = "BA10000stats.dat"
-RPstats = "RP10000stats.dat"
+BAstats10000 = "BA10000stats.dat"
+RPstats10000 = "RP10000stats.dat"
+BAstats1000 = "BA1000stats.dat"
+RPstats1000 = "RP1000stats.dat"
 
 
 
-for file in [BAstats, RPstats]:
+for file in [BAstats1000, RPstats1000]:
     maxdeg = []
     cc = []
     meandegsq = []
@@ -22,8 +24,29 @@ for file in [BAstats, RPstats]:
     print(str(np.mean(meandegsq))+" "+str(np.std(meandegsq)))
     print(str(np.mean(assort))+" "+str(np.std(assort)))
 
+BAgammas1000 = [3.1123800768653913, \
+                3.095087978001225, \
+                3.160539649542988, \
+                3.040319118958632, \
+                3.2348597883839036, \
+                3.048412976237543, \
+                3.142491907826638, \
+                3.111203330231423, \
+                3.1835266001072378, \
+                3.185000045774284]
 
-BAgammas = [3.05389514413736, \
+RPgammas1000 = [3.24599950590404, \
+                 3.251608994673376, \
+                 3.2781568388766362, \
+                 3.370054713203093, \
+                 3.214674752342548, \
+                 3.566998392803999, \
+                 3.573539351619294, \
+                 3.214892638535086, \
+                 3.2581912933365724, \
+                 3.4606165852120054]
+
+BAgammas10000 = [3.05389514413736, \
             2.961057482239264, \
             3.011289537059016, \
             2.9966100688865573, \
@@ -34,7 +57,7 @@ BAgammas = [3.05389514413736, \
             3.179602603228184, \
             3.175947987344969]
 
-RPgammas = [3.233190573749609, \
+RPgammas10000 = [3.233190573749609, \
             3.244511062753258, \
             3.263519395494756, \
             3.1909192869879544, \
@@ -45,7 +68,7 @@ RPgammas = [3.233190573749609, \
             3.2103548174474112, \
             3.1865493246091035]
 
-print(np.mean(BAgammas))
-print(np.std(BAgammas))
-print(np.mean(RPgammas))
-print(np.std(RPgammas))
+print(np.mean(BAgammas1000))
+print(np.std(BAgammas1000))
+print(np.mean(RPgammas1000))
+print(np.std(RPgammas1000))
--- BArankpref/BARank1000.py
+++ BArankpref/BARank1000.py
@@ -1,8 +1,9 @@
 import os
 import re
 import numpy as np
+import networkx as nx
 
-nolinks = ['1','2','3','4','5']
+nolinks = ['3']
 
 file_grow = "experiments/BArankpref/BARankGrow1000.json"
 grow_tmp = re.sub(".json","tmp.json",file_grow)
@@ -10,12 +11,23 @@ file_fit= "experiments/BArankpref/BARankFit.json"
 fit_tmp = re.sub(".json","tmp.json",file_fit)
 dump = "experiments/BArankpref/like.tmp"
 results = "experiments/BArankpref/BARank1000-NUM-results.dat"
+similarities = "experiments/BArankpref/BARank1000Similarities.dat"
 
 numExperiments=10
 betas = np.linspace(0.0,1.0,num=11)
 
 with open(file_grow,'r') as fgrow:
     growdata = fgrow.read()
+    fgrow.close()
+
+def getSimilarity(net):
+    degrees = [net.degree(node) for node in net.nodes()]
+    rankpowers = [np.power(i+1, -0.5) for i in range(len(net.nodes()))]
+    numerator = sum([degrees[j]*rankpowers[j] for j in range(len(net.nodes))])
+    den1 = np.sqrt(sum([rankpowers[j]*rankpowers[j] for j in range(len(rankpowers))]))
+    den2 = np.sqrt(sum([degrees[j]*degrees[j] for j in range(len(degrees))]))
+    denominator = den1*den2
+    return numerator/denominator
 
 for ex in range(numExperiments):
     for link in nolinks:
@@ -41,6 +53,24 @@ for ex in range(numExperiments):
                 f2.write(tmp3)
                 f2.close()
             os.system("java -jar feta3-1.0.0.jar "+fit_tmp+" > "+dump)
+
+            file = "experiments/BArankpref/BARank-1000-"+link+"-"+str(beta)+".dat"
+
+            G = nx.Graph()
+            with open(file,'r') as f:
+                while True:
+                    line = f.readline()
+                    if not line:
+                        break
+                    n1, n2 = line.split()[0], line.split()[1]
+                    G.add_edge(n1,n2)
+                f.close()
+            similarity = getSimilarity(G)
+
+            with open(similarities,'a+') as sfile:
+                sfile.write(str(beta)+" "+str(similarity)+"\n")
+                sfile.close()
+
             os.system("rm "+fit_tmp)
 
             with open(dump,'r') as like:
