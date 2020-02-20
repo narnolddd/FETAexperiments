d1f4494e490c894f0df10d8c3a2b99761897f3fc
Naomi <n.a.arnold@qmul.ac.uk>
2020-02-20 11:31:29 +0000

    returning Enron no changepoint results

--- enron/GrowNoCP.py
+++ enron/GrowNoCP.py
@@ -11,10 +11,11 @@ with open(root+"EnronMeasure.json",'r') as f:
     f.close()
 
 for i in range(10):
-    os.system("java -jar feta3-1.0.0.jar "+root+"EnronCopy.json")
+    if i > 1:
+        os.system("java -jar feta3-1.0.0.jar "+root+"EnronCopy.json")
     tmp = re.sub("NUM",str(i),likedata)
     with open(tmpm_file,'w') as f:
         f.write(tmp)
         f.close()
-    os.system("java -jar feta3-1.0.0.jar"+tmpm_file)
+    os.system("java -jar feta3-1.0.0.jar "+tmpm_file)
 
