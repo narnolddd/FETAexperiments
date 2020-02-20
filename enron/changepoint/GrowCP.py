d5c9e1f8157373d466542e502d51e96bb4918f23
Naomi <n.a.arnold@qmul.ac.uk>
2020-02-20 14:59:20 +0000

    cp results enron

--- enron/changepoint/GrowCP.py
+++ enron/changepoint/GrowCP.py
@@ -3,7 +3,7 @@ warnings.filterwarnings("ignore")
 import os
 import re
 
-root = "experiments/enron"
+root = "experiments/enron/"
 tmpm_file = root+"EnronMeasure.tmp"
 
 with open(root+"EnronMeasure.json",'r') as f:
@@ -11,8 +11,7 @@ with open(root+"EnronMeasure.json",'r') as f:
     f.close()
 
 for i in range(10):
-    if i > 1:
-        os.system("java -jar feta3-1.0.0.jar "+root+"changepoint/EnronCopyCP.json")
+    os.system("java -jar feta3-1.0.0.jar "+root+"changepoint/EnronCopyCP.json")
     tmp = re.sub("NUM",str(i),likedata)
     with open(tmpm_file,'w') as f:
         f.write(tmp)
