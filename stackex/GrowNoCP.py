8bfd316b46050b84831ae590c2cc4153bbc41b49
Naomi Arnold <n.a.arnold@qmul.ac.uk>
2020-03-10 14:46:18 +0000

    stack exchange

---
+++ stackex/GrowNoCP.py
@@ -0,0 +1,20 @@
+import warnings
+warnings.filterwarnings("ignore")
+import os
+import re
+
+root = "experiments/stackex/"
+tmpm_file = root+"MeasureNoCP.tmp"
+
+with open(root+"MeasureNoCP.json",'r') as f:
+    likedata = f.read()
+    f.close()
+
+for i in range(10):
+    os.system("java -jar feta3-1.0.0.jar "+root+"CopyNoCP.json")
+    tmp = re.sub("NUM",str(i),likedata)
+    with open(tmpm_file,'w') as f:
+        f.write(tmp)
+        f.close()
+    os.system("java -jar feta3-1.0.0.jar "+tmpm_file)
+
