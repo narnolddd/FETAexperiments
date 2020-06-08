83d7672833b01d7d3db9b1649cddb10d678fe367
Naomi Arnold <n.a.arnold@qmul.ac.uk>
2020-06-08 16:31:56 +0100

    fb experiments

---
+++ facebook/changepoint/GrowCP.py
@@ -0,0 +1,20 @@
+import warnings
+warnings.filterwarnings("ignore")
+import os
+import re
+
+root = "experiments/facebook/changepoint/"
+tmpm_file = root+"MeasureCP.tmp"
+
+with open(root+"MeasureCP.json",'r') as f:
+    likedata = f.read()
+    f.close()
+
+for i in range(10):
+    print(i)
+    os.system("java -jar feta3-1.0.0.jar "+root+"CopyCP.json")
+    tmp = re.sub("NUM",str(i),likedata)
+    with open(tmpm_file,'w') as f:
+        f.write(tmp)
+        f.close()
+    os.system("java -jar feta3-1.0.0.jar "+tmpm_file)
